cd1cf41238dc65a6da30a1408669eb67f254e27b
julien-padovani <35230682+julien-padovani@users.noreply.github.com>
2018-01-08 19:39:30 +0100

    Add files via upload

---
+++ AzureRmVMStartAll.ps1
@@ -0,0 +1,33 @@
+﻿<#
+.Synopsis
+   Start all the VMs in the Azure Subscription
+.DESCRIPTION
+   All VMs are started in parrelel with Powershell Workflows. 
+   Enter subscription credentials at prompt. 
+   Token is saved in a temp file, used for workflow (parrelal tasks), its removed at the end. 
+.EXAMPLE
+   .\AzureRmVMStartAll.ps1
+#>
+
+$credentialsPath = "$env:TEMP\azureprofile.json"
+
+Login-AzureRmAccount
+Save-AzureRmProfile -Path $credentialsPath -Force
+
+workflow StartVMs {
+    param($VMs, $credentialsPath)
+    foreach -parallel ($vm in $VMs){
+        Select-AzureRmProfile -Path $credentialsPath
+        Start-AzureRmVM -ResourceGroupName $vm.ResourceGroupName -Name $vm.name
+    }
+}
+
+#get VM list
+$VMs = Get-AzureRmVM 
+
+#Start VMs
+StartVMs $VMS $credentialsPath
+
+#clean temp file
+Remove-item $credentialsPath
+
